9b0c6df3a79139e143c214c85c18f9562f65527c
sebastian7722 <sebasti1001@gmail.com>
2025-05-08 18:40:28 +0200

    feat: page link to foods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { getCurrentUser } from "@/auth/currentUser";
 import SignOut from "@/components/auth/sign-out";
-import Searchbar from "@/components/searchbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getCurrentUser({ withFullUser: true });
 
   return (
-    <div>
-      <div className="max-w-lg mx-auto mt-8">
-        <Searchbar />
-      </div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-4">
+      <Button size="lg" asChild>
+        <Link href="/food">Food</Link>
+      </Button>
       <h1 className="text-zinc-50">Hello world</h1>
       <p className="text-zinc-200">{JSON.stringify(session, null, 2)}</p>
       <SignOut />
